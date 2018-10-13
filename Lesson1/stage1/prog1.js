6c20dafbb865d73f17d583d19b383740c25ad2c2
JekaLion <djoni-01@mail.ru>
2018-10-13 13:59:53 +0600

    Signed-off-by: JekaLion <djoni-01@mail.ru>

---
+++ Lesson1/stage1/prog1.js
@@ -0,0 +1,33 @@
+// var alertMessage = "Hello world!";
+// var number=8;
+// number++;
+// number = number + number * number * number / number;
+// alert(alertMessage);
+// alert(number);
+
+// var name = prompt("Введите имя");
+// alert("Ваше имя: " + name);
+
+// var delFile = confirm("Delete file ?");
+// if(delFile == true)
+// {
+//   alert("if is work")
+// }
+// else
+// {
+//   alert("LUL")
+// }
+
+// var arr = [1,2,3,4,5]
+// for (var i = 0; i < arr.length; i++)
+// {
+//   alert(arr[i]);
+// }
+
+var wsx = showMsg("wsx")
+function showMsg(msg)
+{
+  alert(msg);
+  return msg + " complete";
+}
+showMsg(wsx);
